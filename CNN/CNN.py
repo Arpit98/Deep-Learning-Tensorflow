6314354cf29ceff04619841bd49b50ef0ec08363
Arpit Patel <15bce082@nirmauni.ac.in>
2019-03-25 15:20:23 +0530

    - CNN Code Started Need Data set.

--- CNN/CNN.py
+++ CNN/CNN.py
@@ -57,7 +57,7 @@ test_data = create_test_data()
 # If dataset is already created
 # train_data = np.load('train_data.npy')
 # test_data = np.load('test_data.npy')
-train = train_data[-:500]
+train = train_data[:-500]
 test = train_data[-500:]
 X_train = np.array([i[0] for i in train]).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 y_train = [i[1] for i in train]
@@ -80,12 +80,9 @@ convnet = max_pool_2d(convnet, 5)
 convnet = fully_connected(convnet, 1024, activation='relu')
 convnet = dropout(convnet, 0.8)
 convnet = fully_connected(convnet, 2, activation='softmax')
-convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name=)
+convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='output')
 model = tflearn.DNN(convnet, tensorboard_dir='log', tensorboard_verbose=0)
-model.fit({'input': X_train}, {'targets': y_train}, n_epoch
-10,
-validation_set = ({'input': X_test}, {'targets': y_test}),
-                 snapshot_step = 500, show_metric = True, run_id = MODEL_NAME)
+model.fit({'input': X_train}, {'targets': y_train}, n_epoch=10, validation_set = ({'input': X_test}, {'targets': y_test}), snapshot_step = 500, show_metric = True, run_id = MODEL_NAME)
 
 fig = plt.figure(figsize=(16, 12))
 
@@ -108,4 +105,4 @@ for num, data in enumerate(test_data[:16]):
     plt.title(str_label)
     y.axes.get_xaxis().set_visible(False)
     y.axes.get_yaxis().set_visible(False)
-plt.slow()
+plt.show()
